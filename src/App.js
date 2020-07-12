6e816a0016e0ce24c45310b147dc1e08bda316f0
Chris Bragg <cbragg9@yahoo.com>
2020-07-12 12:26:01 -0700

    state moved to App

--- src/App.js
+++ src/App.js
@@ -2,13 +2,26 @@ import React from "react";
 import Form from "./components/Form.js";
 import Header from "./components/Header.js";
 
-function App() {
-  return (
-    <div className="text-center">
-      <Header />
-      <Form />
-    </div>
-  )
+class App extends React.Component {
+  state = {
+    searchInput: ""
+  };
+
+  handleInputChange = event => {
+    this.setState({
+      searchInput: event.target.value
+    });
+  };
+
+  render() {
+    return (
+      <div className="text-center">
+        <Header />
+        <Form handleInputChange={this.handleInputChange} currentState={this.state}/>
+      </div>
+    )
+  }
+
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,24 +2,15 @@ import React, { Component } from "react";
 import "../styles/Form.css";
 
 class Form extends Component {
-  state = {
-    name: ""
-  };
-
-  handleInputChange = event => {
-    this.setState({
-      name: event.target.value
-    });
-  };
 
   render() {
     return (
       <div className="form-box">
         <form className="form-group justify-content-center d-flex">
           <input
-            value={this.state.name}
-            name="name"
-            onChange={this.handleInputChange}
+            value={this.props.currentState.searchInput}
+            name="searchInput"
+            onChange={this.props.handleInputChange}
             type="text"
             placeholder="Search"
             className="form-control"
