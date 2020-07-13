5bb56c77860ab3fca30d17ce8fc2dbe9aec1cd68
Chris Bragg <cbragg9@yahoo.com>
2020-07-12 21:30:46 -0700

    red sort

--- src/components/TableHeader.js
+++ src/components/TableHeader.js
@@ -6,19 +6,19 @@ function TableHeader(props) {
     <tr>
       <th scope="col">Image</th>
       <th scope="col" className="sortBy" onClick={props.handleSort} id="name">
-        <i className={props.sortedBy === "name" ? "fas fa-sort" : "fas fa-sort hidden"}></i>
+        <i className={props.sortedBy === "name" ? "fas fa-sort red" : "fas fa-sort hidden"}></i>
         Name
       </th>
       <th scope="col" className="sortBy" onClick={props.handleSort} id="phone">
-        <i className={props.sortedBy === "phone" ? "fas fa-sort" : "fas fa-sort hidden"}></i>
+        <i className={props.sortedBy === "phone" ? "fas fa-sort red" : "fas fa-sort hidden"}></i>
         Phone
       </th>
       <th scope="col" className="sortBy" onClick={props.handleSort} id="email">
-        <i className={props.sortedBy === "email" ? "fas fa-sort" : "fas fa-sort hidden"}></i>
+        <i className={props.sortedBy === "email" ? "fas fa-sort red" : "fas fa-sort hidden"}></i>
         Email
       </th>
       <th scope="col" className="sortBy" onClick={props.handleSort} id="dob">
-        <i className={props.sortedBy === "dob" ? "fas fa-sort" : "fas fa-sort hidden"}></i>
+        <i className={props.sortedBy === "dob" ? "fas fa-sort red" : "fas fa-sort hidden"}></i>
         DOB
       </th>
     </tr>
